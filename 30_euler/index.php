425700c8815925abcd303bdf0c817b6e66b91d57
Ivon Magon <imagon@jehann.fr>
2020-10-29 16:30:33 +0100

    euler maj

--- 30_euler/index.php
+++ 30_euler/index.php
@@ -3,22 +3,31 @@
 // composer require phpunit/php-timer
 // composer require symfony/var-dumper
 
+/*
+ * ici on se propose de chercher la somme des multiple de 3 ou de 5 en partant de 0 jusqu'à $n
+ * avec un algorythme performant
+ */
+
 use SebastianBergmann\Timer\Timer;
 
 require_once '../vendor/autoload.php';
 
+//démarrage d'un timer
 $timer = new Timer();
 $timer->start();
 
 $n = 10**8;
 $s = 0;
 
+
+// premier algo
 /*for ($i=0; $i<$n; $i++){
     if ($i%3 === 0 || $i%5 === 0) {
         $s+=$i;
     }
 }*/
 
+// deuxième algo un peu plus performant
 for ($i=3; $i<$n; $i+=3) {
     $s+=$i;
 }
@@ -28,6 +37,7 @@ for ($j=5; $j<$n; $j+=5) {
 
 dump($s);
 
+// arrêt du timer et récupération de la durée
 $duration = $timer->stop();
 dump($duration);
 
