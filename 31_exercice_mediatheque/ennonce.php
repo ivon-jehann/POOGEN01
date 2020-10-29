1db0e0402a885ffd0f99ed968190419df4e29b73
Ivon Magon <imagon@jehann.fr>
2020-10-29 16:40:05 +0100

    ennoncé médiathèque

---
+++ 31_exercice_mediatheque/ennonce.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Reprendre l'exercice pdo avec les films et ajouter sur le même modèle que la gestion des films,
+ * une gestion de livres.
+ *
+ * créer des répertoires pour ranger les controlleur, les modèles, les vue
+ * créer les nouvelles classes et nouvelles vues
+ * adapter le point d'entré (fichier index.php) pour pouvoir afficher les pages de la videotheque
+ * et celles de la bibliotheque
+ *
+ */
